685915443e10a294b2a5cd01fa2feb083d305bcd
congdng <92070921+congdng@users.noreply.github.com>
2023-07-08 13:42:30 +0700

    Update KNN

--- model/knn/knn_convert.py
+++ model/knn/knn_convert.py
@@ -12,25 +12,33 @@ import tensorflow_addons as tfa
 from keras.applications.resnet import ResNet50, preprocess_input
 # from keras.applications.vgg16 import VGG16, preprocess_input
 # from keras.applications.inception_v3 import InceptionV3, preprocess_input
+import gdown
+import configparser
+import os
 
-lr = 9.276511e-06
+CONFIG = '../cnn/static/model_download_link.cfg'
+output = '../cnn/static/cnn_model.h5'
+JSONFILE = '../../../dataset_build/Crawl_Data/summary.json'
+EMBEDDINGS = '../cnn/static/embeddings.json'
+lr = 0.001
 wd = lr * 1e-2
-TRAINNO = 312184
-VALIDNO = 52489
-TRAINFOLDER = 'E:/Project/train/image/'
-VALIDFOLDER = 'E:/Project/validation/image/'
-TRAINJSONPATH = 'finalsum.json'
-VALIDJSONPATH = 'finalvalidsum.json'
-TRAINSAVEFOLDER = 'traindata/'
-TESTSAVEFOLDER = 'testdata/'
-FAST_RUN = False
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
 IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
 IMAGE_CHANNELS = 3
 
 #Run the similarity calculation algorithm (K-NN)
- 
+
+def read_config(file):
+    config = configparser.ConfigParser()
+    config.readfp(open(file ,'r'))
+    model_path = config.get('model', 'CNN')
+    model_path = model_path[1:-1]
+    return f'https://drive.google.com/uc?id={model_path}'
+
+def model_download(link, output):
+    gdown.download(link, output, quiet=False)
+    
 def get_embedding(model, imagename):
     img = load_img(imagename, target_size=(IMAGE_WIDTH, IMAGE_HEIGHT))
     x   = img_to_array(img)
@@ -38,7 +46,12 @@ def get_embedding(model, imagename):
     x   = preprocess_input(x)
     return model.predict(x).reshape(-1)
 
-with open('notedarrayresnetfinaldataset.json', 'r') as f:
+#---Main---
+model_link = read_config(CONFIG)
+isExist = os.path.exists(output)
+if (isExist == False):
+    model_download(model_link, output)
+with open(EMBEDDINGS, 'r') as f:
     temp = json.loads(f.read())
     error = []
     for i in temp:
@@ -63,10 +76,9 @@ with open('notedarrayresnetfinaldataset.json', 'r') as f:
                     })
                 except:
                     error.append(s)
-    print(df)
     print(error)
 
-restored_model = tf.keras.models.load_model('Model/cnn.h5')
+restored_model = tf.keras.models.load_model(output)
 secondmodel = Model(inputs = restored_model.input,
                                  outputs= restored_model.layers[3].output)
 print(secondmodel.summary())
@@ -82,5 +94,6 @@ for i in range(len(clothes_categories)):
     neighbors.fit(df_embs)
     select = select.iloc[0:0]
     #print(select)
-    knnPickle = open('clothes_categories[i - 1] + '/finalresnet.pkl', 'wb')
+    sub_category = clothes_categories[i - 1]
+    knnPickle = open(f'data/{sub_category}_knn.pkl', 'wb')
     pickle.dump(neighbors, knnPickle)
